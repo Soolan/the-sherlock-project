790e27c6a4ed2d3df769383097389e5dd5fc5d7f
Sohail <info@soolan.com>
2016-09-17 22:27:15 +0800

    add collector and navigation components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {NavigationComponent} from "./navigation/navigation.component";
+import {CollectorComponent} from "./collector/collector.component";
 
 @Component({
   selector   : 'app',
   templateUrl: './app.html',
+  directives: [NavigationComponent, CollectorComponent]
 })
 export class AppComponent {
 }
