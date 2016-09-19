118a5fa76516cfba6def2d74b81fa86c3f736d2d
Sohail <info@soolan.com>
2016-09-19 17:37:41 +0800

    add service

--- src/app/collector/collector.component.ts
+++ src/app/collector/collector.component.ts
@@ -1,8 +1,19 @@
 import {Component} from '@angular/core';
-
+import {CollectorService} from './collector.service';
 @Component({
   selector: 'sh-collector',
-  templateUrl: './collector.html'
+  templateUrl: './collector.html',
+  providers: [CollectorService]
 })
 export class CollectorComponent {
+  caption = "Some news worth investigating";
+  headlines;
+
+  constructor(collectorService: CollectorService) {
+    this.headlines = collectorService.getHeadlines();
+  }
+
+  onChange(item, status){
+    console.log(item, status);
+  }
 }
---
+++ src/app/collector/collector.service.ts
@@ -0,0 +1,10 @@
+export class CollectorService {
+  getHeadlines(): string[] {
+    return [
+      "Lorem ipsum dolor sit amet",
+      "consectetuer adipiscing elit",
+      "Integer tincidunt Cras dapibus",
+      "Quia dolori non voluptas "
+    ];
+  }
+}
