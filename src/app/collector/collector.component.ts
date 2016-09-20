b7c85826217341b9c940087738a8bdfa7244dbed
Sohail <info@soolan.com>
2016-09-20 09:17:24 +0800

    add firebase database and record selected news

--- src/app/collector/collector.component.ts
+++ src/app/collector/collector.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {CollectorService} from './collector.service';
+import {FirebaseListObservable, AngularFire} from "angularfire2";
 @Component({
   selector: 'sh-collector',
   templateUrl: './collector.html',
@@ -9,11 +10,19 @@ export class CollectorComponent {
   caption = "Some news worth investigating";
   headlines;
 
-  constructor(collectorService: CollectorService) {
-    this.headlines = collectorService.getHeadlines();
+  private items: FirebaseListObservable<any>;
+
+  constructor (collectorService: CollectorService, af: AngularFire) {
+    collectorService.getHeadlines()
+      .subscribe(
+        data => {
+          this.headlines = data;
+        }
+      );
+    this.items = af.database.list('/Collector');
   }
 
-  onChange(item, status){
-    console.log(item, status);
+  onChange(item){
+    this.items.push(item);
   }
 }
