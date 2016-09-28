47464fb4caac4a8309cae21d54fd1b057c3664d3
Sohail <info@soolan.com>
2016-09-28 17:33:27 +0800

    add logic + pipe + template

--- src/app/rating/rating.logic.ts
+++ src/app/rating/rating.logic.ts
@@ -1,13 +1,47 @@
+import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+
+@Injectable()
 export class RatingLogic {
-  //ToDo: construct the service object
-  constructor() {}
+  private url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20%20from%20json%20where%20url%3D%22http%3A%2F%2Fhawttrends.appspot.com%2Fapi%2Fterms%2F%22&format=json&diagnostics=true&callback=';
+  private http;
+  private trends;
 
-  //ToDo: Fetch the latest trends from the given URL
-  getTrends () {}
+  constructor(http: Http) {
+    this.http = http;
+  }
 
-  //ToDo: Rate a given news item based on the trends
-  rateTrends() {}
+  getTrends (count) {
+    var trends = [];
+    this.http.get(this.url)
+      .toPromise()
+      .then(res => res.json())
+      .then(data => data.query.results.json._)
+      .then(function(data) {
+        for (var i=0; i<count; i++)
+          trends.push({'keyword':data[i], 'rank': count-i});
+      });
+    this.trends = trends;
+    return trends;
+  }
 
-  //ToDo: Rate a given news item based on the published date
-  rateDate() {}
+  rateTrends(newsItem) {
+    var trendsRank = 0;
+    this.trends.forEach(
+      function (trend) {
+        if(newsItem.indexOf(trend.keyword)>0){
+          trendsRank += trend.rank;
+        }
+      });
+    return trendsRank;
+  }
+
+  rateDate(newsDate) {
+    var last24Hours = 86400000; // 24*60*60*1000
+    var now = new Date().getTime();
+    var then= new Date(newsDate).getTime();
+    if((now - then) < last24Hours)
+      return 1;
+    return 0;
+  }
 }
--- src/app/rating/rating.service.ts
+++ src/app/rating/rating.service.ts
@@ -1,16 +1,54 @@
 import {Injectable} from "@angular/core";
+import {
+  FirebaseListObservable, AngularFire
+} from "angularfire2/angularfire2";
+import {RatingLogic} from "./rating.logic";
 
 @Injectable()
 export class RatingService {
-  //ToDo: construct the service object
-  constructor() {}
+  private news: FirebaseListObservable<any>;
+  private rl;
+  private trends;
+  protected numberOfTrends = 20;
 
-  //ToDo: get the collected news from Firebase db
-  getNews() {}
+  constructor(af: AngularFire, rl: RatingLogic) {
+    this.news = af.database.list('/Collector');
+    this.rl   = rl;
+    this.trends =rl.getTrends(this.numberOfTrends)
+  }
 
-  //ToDo: get the latest trends to show them inside the template
-  getTrends() {}
+  getNews() {
+    return this.news;
+  }
 
-  //ToDo: rate the news based on constructed rules
-  rateNews(){}
+  getTrends() {
+    return this.trends;
+  }
+
+  rateNews() {
+    var news = [];
+    var self = this;
+    // first get a reference to the value part of news object
+    this.news._ref.once("value")
+    // then loop through each news item
+      .then(snapshots => { snapshots.forEach(
+        function(snapshot) {
+          var newsRank;
+          // save individual news components in a set of variables
+          var title = snapshot.child("title").val();
+          var desc = snapshot.child("description").val();
+          var date = snapshot.child("pubDate").val();
+          var link = snapshot.child("link").val();
+          // calculate the news rank
+          newsRank = self.rl.rateTrends(title+' '+desc) + self.rl.rateDate(date);
+          // push the ranked news into an array of news objects
+          news.push({
+            'title':title,
+            'descrition': desc,
+            'rank': newsRank,
+            'link': link
+          });
+        })});
+    return news;
+  }
 }
