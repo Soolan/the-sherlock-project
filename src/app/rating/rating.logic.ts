bffa40329d3989d642146d46e143f49ef49192ec
Sohail <info@soolan.com>
2016-09-28 13:33:17 +0800

    add an empty rating logic

---
+++ src/app/rating/rating.logic.ts
@@ -0,0 +1,13 @@
+export class RatingLogic {
+  //ToDo: construct the service object
+  constructor() {}
+
+  //ToDo: Fetch the latest trends from the given URL
+  getTrends () {}
+
+  //ToDo: Rate a given news item based on the trends
+  rateTrends() {}
+
+  //ToDo: Rate a given news item based on the published date
+  rateDate() {}
+}
---
+++ src/app/rating/rating.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class RatingService {
+  //ToDo: construct the service object
+  constructor() {}
+
+  //ToDo: get the collected news from Firebase db
+  getNews() {}
+
+  //ToDo: get the latest trends to show them inside the template
+  getTrends() {}
+
+  //ToDo: rate the news based on constructed rules
+  rateNews(){}
+}
