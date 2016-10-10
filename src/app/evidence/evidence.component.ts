6560f05987c2fd66e14eeb44df2ff18c6120ea55
Sohail <info@soolan.com>
2016-10-10 17:44:37 +0800

    add evidence component + service

--- src/app/evidence/evidence.component.ts
+++ src/app/evidence/evidence.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
+import {EvidenceService} from "./evidence.service";
 
 @Component({
   selector: 'sh-evidence',
   templateUrl: 'evidence.html'
 })
 export class EvidenceComponent {
+  private evidenceService:EvidenceService;
 
+  constructor (es:EvidenceService) {
+    this.evidenceService = es;
+  }
 }
