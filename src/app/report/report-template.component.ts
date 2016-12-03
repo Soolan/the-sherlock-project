91b54d86196ee9eaffabedd79d4f9afdc6d5b7d4
Sohail <info@soolan.com>
2016-12-03 11:35:12 +0800

    child template wip

--- src/app/report/report-template.component.ts
+++ src/app/report/report-template.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, EventEmitter, Output} from '@angular/core';
 import {ReportConfig} from "./report.config";
 
 @Component({
@@ -6,6 +6,19 @@ import {ReportConfig} from "./report.config";
   templateUrl: './report-template.html'
 })
 export class ReportTemplateComponent {
-  @Input('templates') model: ReportConfig;
+  @Input('template') model: ReportConfig;
+  private headingId = 'myId';
+  constructor () {
+    // console.log(this.ids);
+    // console.log(template);
+  }
+  // private headingId = this.model.$key;
+  // private collapseId;
+  // @Output() onNewReport = new EventEmitter<ReportConfig>();
 
+  // newReport() {
+  //   this.onNewReport.emit( new ReportConfig('untitled', false,
+  //     false, false, 0, 0, false, false, false, false, false,
+  //     {show:true, size:false, distance:false, url:false}, 0, 0));
+  // }
 }
--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -8,24 +8,42 @@ import {ReportConfig} from "./report.config";
   templateUrl: './report.html'
 })
 export class ReportComponent implements OnInit {
-  private templates;//: FirebaseListObservable<any>;
+  // private templates;//: FirebaseListObservable<any>;
+  private templates: FirebaseListObservable<any>;
+  private items = [];
   private angularFire;
   private newsItems;
   private reportService;
 
   constructor(/*rs:ReportService, */af: AngularFire) {
     // this.reportService= rs;
-    this.angularFire = af;
+    // this.angularFire = af;
+    this.templates = af.database.list('/Report/templates');
+
+      // .subscribe(data => this.templates);
   }
 
   ngOnInit() {
-    this.templates  = new ReportConfig('test', true, true, false, 5, 6, true, true, false, true, true,
-        {show: true, size: true, distance: true, url: true}, 11, 22);
-  //   this.angularFire.database.list('/Report/templates')
-  //     .subscribe(data => {
-  //       this.templates = data;
-  //     });
+    // this.templates  = new ReportConfig('test', true, true, false, 5, 6, true, true, false, true, true,
+    //     {show: true, size: true, distance: true, url: true}, 11, 22);
+    this.templates.subscribe(data => {
+        this.items = data;
+        console.log('templates:', this.templates);
+        console.log('items:', this.items);
+      });
   }
+
+  newReport() {
+    this.templates.push( new ReportConfig('untitled333', false,
+        false, false, 0, 0, false, false, false, false, false,
+        {show:true, size:false, distance:false, url:false}, 0, 0)
+    );
+  }
+
+  // onNewReport( report: ReportConfig) {
+  //   console.log(report);
+  //   this.templates.push(report);
+  // }
 
   // 1. read templates from db
   // 2. save them in an array
