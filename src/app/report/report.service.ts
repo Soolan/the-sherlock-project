1219b9c1cd780e79d947d36609f868cad0eb7d59
Sohail <info@soolan.com>
2016-12-10 09:22:57 +0800

    report service - wip

--- src/app/report/report.service.ts
+++ src/app/report/report.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Injectable()
@@ -15,6 +15,9 @@ export class ReportService{
   setCorpus(template) {
     let topFreqs = [];
     let topIDFs = [];
+    let article = {};
+    article = this.setArticles(template);
+
     // calculating topFreqs
     this.angularFire.database.list('Evidence/Corpus/IDFs', {
       query: {
@@ -35,7 +38,8 @@ export class ReportService{
       }})
       .subscribe(data => {
         data.forEach( d => {
-          topIDFs.push({word:d.word, idf: d.IDF.toFixed(7);})
+          topIDFs.push({word:d.word, idf: d.IDF.toFixed(7)});
+
         });
       });
     return {
@@ -45,6 +49,36 @@ export class ReportService{
       averageSize: 759
     };
   }
+
+  setArticles(template) {
+    this.angularFire.database.list('Evidence/Corpus/Articles', {
+      query: {
+        orderByChild: "bag_of_words"
+        // limitToFirst: 5 // highest occurrence
+      }})
+      .subscribe(data => {
+        console.log('Longest/shortest ARTICLES:',
+          data[data.length - 1].article.length,
+          data[0].article.length,
+          data[data.length - 1].article.link,
+          data[0].article.link,
+          data[data.length - 1].article,
+          data[0].article
+        );
+        let sum = 0;
+        data.forEach( d => {
+          sum += d.article.length;
+        });
+        console.log('Avg ARTICLE:', sum, data.length, sum/data.length);
+      });
+    return null;
+  }
+  // sort function
+  /*
+   .sort(function (a, b) {
+   return a['article'].length - b['article'].length;
+   });
+   */
 
   setCluster(template) {
     // ToDo: Initialize related values
