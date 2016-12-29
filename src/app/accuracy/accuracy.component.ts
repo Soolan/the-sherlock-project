81bab8970cd56678cc07d205f6af719262f52433
Sohail <info@soolan.com>
2016-12-29 19:54:40 +0800

    add cure handling functions to the component

--- src/app/accuracy/accuracy.component.ts
+++ src/app/accuracy/accuracy.component.ts
@@ -1,21 +1,36 @@
 import {Component, OnInit} from '@angular/core';
-import {AngularFire} from "angularfire2";
+import {AngularFire, FirebaseListObservable} from "angularfire2";
 
 @Component({
   selector: 'sh-accuracy',
-  templateUrl: './accuracy.html'
+  templateUrl: './accuracy.html',
+  styleUrls: ['./accuracy.css']
 })
 export class AccuracyComponent implements OnInit {
   private prevents = [];
+  private cures: FirebaseListObservable<any>;
   private angularFire;
   constructor(af: AngularFire) {
     this.angularFire = af;
   }
+
   ngOnInit() {
     this.angularFire.database
-      .list('/Accuracy/prevents')
+      .list('/Accuracy/cures')
       .subscribe(data => {
-        this.prevents = data
+        this.cures = data
       })
+  }
+
+  newCure() {
+    this.cures.push({description:'new cure'})
+  }
+
+  editCure(event, id) {
+    this.cures.update(id, {description:event.target.outerText});
+  }
+
+  deleteCure(id) {
+    this.cures.remove(id);
   }
 }
